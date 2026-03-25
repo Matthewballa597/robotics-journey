a10d8d9b83cc2a6f9facf1bdb399ec659c9ef63f
Matthewballa597 <144379753+Matthewballa597@users.noreply.github.com>
2026-03-25 13:24:00 +0100

    Add 3 LED variable control circuit - Day 4 project

---
+++ projects/led-variable-control/code.ino
@@ -0,0 +1,174 @@
+int redLED=8;
+int blueLED=9;
+int yellowLED=10;
+
+int fast= 100;
+int slow= 300;
+int longW= 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLED,OUTPUT);
+  pinMode(blueLED,OUTPUT);
+  pinMode(yellowLED,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(redLED,HIGH);
+  delay(fast);
+  digitalWrite(redLED,LOW);
+  delay(fast);
+
+  digitalWrite(redLED,HIGH);
+  delay(fast);
+  digitalWrite(redLED,LOW);
+  delay(fast);
+
+  digitalWrite(redLED,HIGH);
+  delay(fast);
+  digitalWrite(redLED,LOW);
+  delay(fast);
+
+  digitalWrite(redLED,HIGH);
+  delay(fast);
+  digitalWrite(redLED,LOW);
+  delay(fast);
+
+  digitalWrite(redLED,HIGH);
+  delay(fast);
+  digitalWrite(redLED,LOW);
+  delay(fast);
+//stop for red;
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ digitalWrite(blueLED,HIGH);
+ delay(slow);
+ digitalWrite(blueLED,LOW);
+ delay(slow);
+
+ // stop blue
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+ digitalWrite(yellowLED,HIGH);
+ delay(longW);
+ digitalWrite(yellowLED,LOW);
+ delay(longW);
+
+
+
+}
